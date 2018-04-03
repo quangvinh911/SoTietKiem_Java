1855ccab2a0e3bd6ac28023fac8a989e02cda2dd
ThuOanh229 <thuoanh229@gmail.com>
2018-04-04 00:11:55 +0700

    SoTietKiem

--- src/controller/SoTietKiemController.java
+++ src/controller/SoTietKiemController.java
@@ -7,7 +7,18 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.Khachhang;
+import model.Loaitietkiem;
+import model.Sotietkiem;
+import org.hibernate.SessionFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +27,35 @@ import javafx.fxml.Initializable;
  */
 public class SoTietKiemController implements Initializable {
 
+    private SessionFactory factory;
+    private ObservableList<Sotietkiem> idstk;
+    @FXML
+    private TextField txtMaSoTietKiem;
+    @FXML
+    private TextField txtSoTienGui;
+    @FXML
+    private Label lblTongTien;
+    @FXML
+    private TextField txtLoaiTien;
+    @FXML
+    private DatePicker dpNgayMo;
+    @FXML
+    private DatePicker dpNgayTinhLai;
+    @FXML
+    private DatePicker dpNgayDenHan;
+    @FXML
+    private ComboBox<Khachhang> cbMaKhachHang;
+    @FXML
+    private ComboBox<Loaitietkiem> cbMaLoaiTietKiem;
+    @FXML
+    private CheckBox ckbDongSo;
     /**
      * Initializes the controller class.
      */
     @Override
+
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
